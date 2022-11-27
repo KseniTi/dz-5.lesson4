a100617430fd508668d58a9b1f91416d5248faea
KseniTi <lioness2006@bk.ru>
2022-11-27 14:14:54 +0300

    Prog027

---
+++ Prog027/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на
+вход число и выдаёт сумму цифр в числе
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+void Zadacha27()
+{
+    Random rand = new Random();
+    int number = rand.Next(1, 10);
+    Console.WriteLine("Число: " + number);
+
+    string numberStr = Convert.ToString(number);
+
+    int sum = 0;
+    for(int i = 0; i < numberStr.Length; i++)
+    {
+        sum += numberStr[i];
+    }
+    Console.WriteLine(sum); // не могу понять почему тут результат выходит не верный
+}
+Zadacha27();
+

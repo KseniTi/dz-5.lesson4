a485196916c4eca3fedbdabafc58adcb94890c4f
KseniTi <lioness2006@bk.ru>
2022-11-27 13:44:59 +0300

    Prog025

---
+++ Prog025/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Используя определение степени числа,
+напишите цикл, который принимает на вход два
+натуральных числа (A и B) и возводит число A в степень B.
+*/
+void Zadacha25()
+{
+    Random rand = new Random();
+    int numberA = rand.Next(1, 16);
+    int numberB = rand.Next(1, 16);
+    Console.WriteLine("Число А: " + numberA + ". Число В: " + numberB);
+
+    int count = 1;
+    int stepen = 1;
+    while (count <= numberB)
+    {
+        stepen *= numberA;
+        count++;
+    }
+    Console.WriteLine(stepen); 
+}
+
+Zadacha25();
+
